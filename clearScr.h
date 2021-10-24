c3fbf759e1d19ce646f37629ef3bd091e3036274
NicholaxKAmIL <majoajoa@gmail.com>
2021-10-24 13:42:48 +0800

    Start

---
+++ clearScr.h
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+void ClearScreen(){
+	for(int i = 0;i<29;i++)
+	cout<<"O"<<"           "<<"O"<<i<<endl;
+  }
---
+++ logo.h
@@ -0,0 +1,5 @@
+#include<iostream>
+void logo(){
+	std::cout<<"Hi!"<<std::endl;
+
+}
